d2f4fea5c368b0b5963a13ea1531ec78e25f040c
mariansam <margotka000@gmail.com>
2019-10-09 18:25:31 +0200

    add source code

---
+++ Bogosort/Bogosort.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bogosort
+{
+    public static class Bogosorter
+    {
+        /// <summary>
+        /// The best sorting algorithm ever created!
+        /// </summary>
+        /// <param name="list">The list you want to sort</param>
+        /// <typeparam name="T">An IComparable, usally int</typeparam>
+        public static void Bogosort<T>(this IList<T> list) where T : IComparable
+        {
+            if (list is null)
+                throw new ArgumentNullException(nameof(list));
+
+            while (!list.IsSorted())
+                list.Shuffle();
+        }
+
+        private static bool IsSorted<T>(this IList<T> list) where T : IComparable
+        {
+            if (list.Count <= 1)
+                return true;
+            
+            for (int i = 1; i < list.Count; i++)
+                if (list[i].CompareTo(list[i - 1]) < 0)
+                    return false;
+
+            return true;
+        }
+
+        private static void Shuffle<T>(this IList<T> list) where T : IComparable
+        {
+            Random ran = new Random();
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                int index = ran.Next(list.Count);
+                T value = list[index];
+                list[index] = list[i];
+                list[i] = value;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # Bogosort.NET
 The best sorting algorithm in C#!
+
+# Installation
+This should be available as a NuGet package ASAP!
+
+# Usage
+The usage of this package is pretty straight-forward. Here is an example.
+```cs
+using System;
+using System.Collections.Generic;
+using Bogosort;
+
+namespace Bogosort.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>() { 0, 3, 4, 7, 4, 16, 9, 147, 9, 14 };
+            list.Bogosort();
+            Console.WriteLine(string.Join(" ", list));
+        }
+    }
+}
+```
+
+## **Enjoy!**
